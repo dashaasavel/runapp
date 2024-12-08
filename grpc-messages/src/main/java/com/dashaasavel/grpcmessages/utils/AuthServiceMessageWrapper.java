4375c9ab53f76a71a01552569d7b831b3d9412ea
Savelyeva Daria <savelyeva.d@agile-sys.ru>
2024-12-08 20:25:58 +0300

    rm unused code, turn off integration tests in maven-surefire-plugin

--- grpc-messages/src/main/java/com/dashaasavel/grpcmessages/utils/AuthServiceMessageWrapper.java
+++
@@ -1,9 +0,0 @@
-package com.dashaasavel.grpcmessages.utils;
-
-import com.dashaasavel.userservice.api.Authservice;
-
-public class AuthServiceMessageWrapper {
-    public static Authservice.UserCredentials userCredentials(String username, String password) {
-        return Authservice.UserCredentials.newBuilder().setUsername(username).setPassword(password).build();
-    }
-}
--- grpc-messages/src/main/java/com/dashaasavel/grpcmessages/utils/DateUtils.java
+++ grpc-messages/src/main/java/com/dashaasavel/grpcmessages/utils/DateUtils.java
@@ -6,16 +6,9 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 public class DateUtils {
-    public static Date createGrpcDate(int year, int month, int day) {
-        return Date.newBuilder().setYear(year).setMonth(month).setDay(day).build();
-    }
 
     public static Date convertToGrpcDate(LocalDate localDate) {
         return Date.newBuilder().setYear(localDate.getYear()).setMonth(localDate.getMonthValue()).setDay(localDate.getDayOfMonth()).build();
-    }
-
-    public static LocalDate convertToLocalDate(Date date) {
-        return LocalDate.of(date.getYear(), date.getMonth(), date.getDay());
     }
 
     public static DayOfWeek convertToDayOfWeek(com.dashaasavel.date.api.DayOfWeek dayOfWeek) {
