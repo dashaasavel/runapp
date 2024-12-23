22979ff60bd0cde84b0f4089b5b30e453457e7bc
Savelyeva Daria <savelyeva.d@agile-sys.ru>
2024-12-23 19:46:30 +0300

    * rm unused bean

--- userservice/src/main/java/com/dashaasavel/userservice/UserServiceApplication.java
+++ userservice/src/main/java/com/dashaasavel/userservice/UserServiceApplication.java
@@ -1,13 +1,10 @@
 package com.dashaasavel.userservice;
 
-import com.dashaasavel.runapplib.grpc.core.PermittedChannels;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = {
         SecurityAutoConfiguration.class,
@@ -19,11 +16,4 @@ public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
-
-    @Bean
-    @ConfigurationProperties("application.security.permitted-grpc-channels")
-    public PermittedChannels permittedGrpcChannels() {
-        return new PermittedChannels();
-    }
-
 }
