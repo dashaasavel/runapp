3442262434e22aabc4618dacc6e4678b73353423
Savelyeva Daria <savelyeva.d@agile-sys.ru>
2024-12-26 12:56:39 +0300

    refactor properties-files

--- runapp-lib/src/main/java/com/dashaasavel/runapplib/grpc/core/PermittedChannels.kt
+++
@@ -1,5 +0,0 @@
-package com.dashaasavel.runapplib.grpc.core
-
-class PermittedChannels {
-    var list = mutableListOf<String>()
-}
--- runservice/src/main/java/com/dashaasavel/runservice/RunServiceApplication.java
+++ runservice/src/main/java/com/dashaasavel/runservice/RunServiceApplication.java
@@ -1,11 +1,8 @@
 package com.dashaasavel.runservice;
 
-import com.dashaasavel.runapplib.grpc.core.PermittedChannels;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = {
         RabbitAutoConfiguration.class
@@ -14,11 +11,5 @@ public class RunServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RunServiceApplication.class, args);
-    }
-
-    @Bean
-    @ConfigurationProperties("application.security.permitted-grpc-channels")
-    public PermittedChannels permittedGrpcChannels() {
-        return new PermittedChannels();
     }
 }
